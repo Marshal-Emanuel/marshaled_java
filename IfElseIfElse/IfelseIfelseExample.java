c4bcc5e819a9735b6de58b32fba6b50dfb69058b
Marshal-Emanuel <marshcodes@gmail.com>
2022-11-10 19:33:38 +0300

    if else if

---
+++ IfElseIfElse/IfelseIfelseExample.java
@@ -0,0 +1,31 @@
+public class IfelseIfelseExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*if else statement is used to execute multible of actions bassed uppon 
+		 * mu;tible conditions 
+		 * syntax of if else if sataemant is 
+		 * 
+		 * if (<condition1>)
+		 * statement1
+		 * else if (<condition2>)
+		 * statement 2
+		 * 
+		 * else  
+		 * statement 3
+		 * if <contition 1 > is true statement 1 will be executed ,else if <condition 2 >
+		 * is true statement 2 is executed and so on.if no condition is true ,then else 
+		 * statement will be executed.
+		 * 
+		 */
+		int i =10;
+		if (i>100)
+			System.out.println("i is greater then 100");
+		else if (i>50)
+			System.out.println("i is greater then 50");
+		else
+			System.out.println("i is less  then 50");
+
+	}
+
+}
