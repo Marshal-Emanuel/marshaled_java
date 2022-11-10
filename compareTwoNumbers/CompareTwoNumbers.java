e89168347c5ec1426e1d119076ab39c6a3408b55
Marshal-Emanuel <marshcodes@gmail.com>
2022-11-10 19:29:11 +0300

    compare 2 numbers

---
+++ compareTwoNumbers/CompareTwoNumbers.java
@@ -0,0 +1,23 @@
+public class CompareTwoNumbers {
+
+	public static void main(String[] args) {
+		//declare two number to compare
+		int num1=324;
+		int num2=234;
+		if (num1>num2)
+		{
+			System.out.println(num1+" is greater than "+num2);
+			
+		}
+		else if (num1<num2)
+		{
+			System.out.println(num1+"is less than "+num2);
+		}
+		else
+		{
+			System.out.println(num1+"is equal than "+num2);
+		}
+
+	}
+
+}
