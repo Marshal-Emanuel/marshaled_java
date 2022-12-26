d129d85315253779858e2ab6616e1f1e1786abfc
Marshal-Emanuel <marshcodes@gmail.com>
2022-12-26 17:22:05 +0300

    main

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        Pokemon p1=new Pokemon();
+        p1.name="Pikachu";
+        p1.level=10;
+
+        p1.attack();
+    }
+    
+}
