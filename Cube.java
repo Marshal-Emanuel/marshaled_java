a0ff8e226f52b0bf0f6beb25cc91f7d4c9976d2c
Emanuel Marshal <marshcodes@gmail.com>
2023-01-24 01:29:55 +0300

    cube

---
+++ Cube.java
@@ -0,0 +1,11 @@
+public class Cube{
+    public static void main(String[] args){
+        System.out.println(cube(4));
+    }
+
+    public static int cube(int number){
+        
+        return (number*number*number);
+        
+    }
+}
--- Parameters.java
+++ Parameters.java
@@ -1,9 +1,9 @@
 public class Parameters{
     public static void main(String[] args){
-        greet("Whitney");
+        greet("Whitney", 19);
     }
 
-    public static void greet(String name){
-        System.out.println("Hello "+ name);
+    public static void greet(String name, int age){
+        System.out.println("Hello "+ name +". Your age is "+age);
     }
 }
