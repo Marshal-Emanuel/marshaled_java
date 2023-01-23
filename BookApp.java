67aab73f367243a3932ba681f7889a8ad4c0708c
Emanuel Marshal <marshcodes@gmail.com>
2023-01-23 19:29:41 +0300

    parameters

--- BookApp.java
+++ BookApp.java
@@ -1,20 +1,20 @@
 public class BookApp{
     public static void main(String[] args){
         
-        Book book1=new Book();
-        book1.author = "Wallah_Bin_Wallah";
+        Book book1=new Book("Wallah_Bin_Wallah", "Kiswahili_mufti", "Swahili", 30);
+      /*   book1.author = "Wallah_Bin_Wallah";
         book1.title = "Kiswahili_mufti";
         book1.language = "Swahili";
-        book1.pages=300;
+        book1.pages=300;*/
 
 
-        Book book2=new Book();
-        book2.author = "J.k Rowling";
+        Book book2=new Book("J.p Rowling", "Harry Potter", "English", 400 );
+        /*book2.author = "J.k Rowling";
         book2.title = "Harry Porter";
         book2.language = "English";
-        book2.pages=400;
+        book2.pages=400;*/
 
-        System.out.println(book1.author);
+        System.out.println(book2.author);
 
     }
 }
---
+++ Parameters.java
@@ -0,0 +1,9 @@
+public class Parameters{
+    public static void main(String[] args){
+        greet("Whitney");
+    }
+
+    public static void greet(String name){
+        System.out.println("Hello "+ name);
+    }
+}
