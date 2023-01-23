56320d0c51c5b99e2db67a72b82c0a3becbef845
Emanuel Marshal <marshcodes@gmail.com>
2023-01-23 17:28:53 +0300

    bookapp

--- Book.java
+++ Book.java
@@ -2,5 +2,12 @@ public class Book{
     String author;
     String title;
     String language;
-    int pages; 
+    int pages;
+    
+    public Book(String author, String title, String language, int pages){
+        this.author=author;
+        this.title=title;
+        this.language=language;
+        this.pages=pages;
+    }
 }
--- BookApp.java
+++ BookApp.java
@@ -1,10 +1,18 @@
 public class BookApp{
     public static void main(String[] args){
+        
         Book book1=new Book();
         book1.author = "Wallah_Bin_Wallah";
         book1.title = "Kiswahili_mufti";
         book1.language = "Swahili";
         book1.pages=300;
+
+
+        Book book2=new Book();
+        book2.author = "J.k Rowling";
+        book2.title = "Harry Porter";
+        book2.language = "English";
+        book2.pages=400;
 
         System.out.println(book1.author);
 
