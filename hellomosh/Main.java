02c3e1d63fb98b567ef2e0a15a1cb403a42152cf
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-03-13 23:31:13 +0300

    My world

--- hellomosh/Main.java
+++ hellomosh/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world with Marshal");
+        System.out.println("Hello welcome to my world; Marshal");
     }
 }
