df38ca875c9efdf26621ab80c42348ba0f5496ce
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-06-30 15:01:54 +0100

    Update Diamond.java

--- A Recursive Asterisk Diamond Shape/Diamond.java
+++ A Recursive Asterisk Diamond Shape/Diamond.java
@@ -6,7 +6,7 @@ public class Diamond {
 		
 				Scanner input =new Scanner (System.in);
 				int num1;
-				System.out.println("Please enter the number to find the reculsive:");
+				System.out.println("Please e nter the number to find the reculsive:");
 				num1=input.nextInt();
 				System.out.println(Diamond_Asterisk(num1));
 				System.out.println(Diamond_Asterisk2(num1));
