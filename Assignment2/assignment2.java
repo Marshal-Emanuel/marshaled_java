b693018dac689e0139a782b37ef2649263640481
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-09-05 19:02:06 +0300

    Update assignment2.java

--- Assignment2/assignment2.java
+++ Assignment2/assignment2.java
@@ -1,7 +1,7 @@
 public class assignment2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 		System.out.print("this is just an example...");
 
 	}
