49e02bd35349d55837010451acb11ea0786fcbfd
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-09-22 01:14:45 +0300

    Update IfelseIfelseExample.java

--- IfElseIfElse/IfelseIfelseExample.java
+++ IfElseIfElse/IfelseIfelseExample.java
@@ -1,7 +1,7 @@
 public class IfelseIfelseExample {
 	 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub  
+		// TODO Auto-generated method stub 
 		/*if else statement is used to execute multible of actions bassed uppon  
 		 * mu;tible conditions 
 		 * syntax of if else if sataemant is    
