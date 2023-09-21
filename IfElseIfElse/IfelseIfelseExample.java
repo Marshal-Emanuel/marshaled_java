b63fc73f67863cf801ce2e4af0c7d3ee802bd1ca
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-09-22 01:14:38 +0300

    Update IfelseIfelseExample.java

--- IfElseIfElse/IfelseIfelseExample.java
+++ IfElseIfElse/IfelseIfelseExample.java
@@ -1,6 +1,6 @@
 public class IfelseIfelseExample {
 	 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub  
 		/*if else statement is used to execute multible of actions bassed uppon  
 		 * mu;tible conditions 
